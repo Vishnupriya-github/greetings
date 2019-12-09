2c3b167ebf70c2e675a821b381c1db1ac93e7cbf
vishnupriya <gottipati.vishnupriya@gmail.com>
2019-12-09 12:45:07 +0530

    Main Application is added

---
+++ greeting/src/com/greeting/MainApplication.java
@@ -0,0 +1,17 @@
+package com.greeting;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		Greeting greeting = () -> System.out.println("Good Morning");
+		displayGreeting(greeting);
+	}
+	public static void displayGreeting(Greeting greeting) {
+		greeting.greet();
+	}
+}
+
+@FunctionalInterface
+interface Greeting{
+	public void greet();
+}
